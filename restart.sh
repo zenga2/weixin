ed66249f8fb1c33d837e05e3b36263d20101e203
zenga2 <zenga2@foxmail.com>
2018-05-14 15:27:42 +0800

    Optimize shell scripts

--- restart.sh
+++ restart.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
+echo '------------- Ready to start ---------------------'
 kill `pidof node`
 nohup node src/main.js &
 sleep 1
+echo '------------- started successfully --------------------'
 
