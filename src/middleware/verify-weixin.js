51f20c3d65afc2719ba47adc4d168db99491b4b8
zenga2 <zenga2@foxmail.com>
2018-05-14 16:42:04 +0800

    fix bug

--- src/middleware/verify-weixin.js
+++ src/middleware/verify-weixin.js
@@ -5,12 +5,10 @@ module.exports = () => async (ctx, next) => {
   const {request, response} = ctx
   const {signature, timestamp, nonce, echostr} = request.query
 
-  console.log(request.path)
-
   if (signature && timestamp && nonce && echostr) {
     const str = [token, timestamp, nonce].sort().join('')
     const strAfterSha1 = sha1(str)
-    if (strAfterSha1 === echostr) {
+    if (strAfterSha1 === signature) {
       response.body = echostr
     } else {
       response.status = 403
