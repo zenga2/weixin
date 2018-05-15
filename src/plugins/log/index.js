e8d2dc67176c0b245a9b1e06135a79e9610de991
zenga2 <zenga2@foxmail.com>
2018-05-15 14:17:18 +0800

    修改日志逻辑

--- src/plugins/log/index.js
+++ src/plugins/log/index.js
@@ -3,21 +3,28 @@ const fs = require('fs')
 const fsUitls = require('../../utils/fsUtils')
 const dateUtils = require('../../utils/dateUtils')
 
+const logDir = path.join(__dirname, '../../../w_logs')
+
 function resolve(filename) {
-  return path.join(__dirname, '../../../w_logs', filename + '.log.txt')
+  return path.join(logDir, filename + '.log.txt')
 }
 
 async function log(msg) {
   const filename = dateUtils.formate(new Date(), 'yyyyMMdd')
   const fPath = resolve(filename)
 
-  msg = `${dateUtils.formate(new Date(), 'yyyy-MM-dd hh:mm:ss')}  ${msg}`
+  if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir)
+  }
 
+  msg = `${dateUtils.formate(new Date(), 'yyyy-MM-dd hh:mm:ss')}  ${msg}`
   let text = !fs.existsSync(fPath)
     ? msg
     : await fsUitls.readFile(fPath) + '\n' + msg
 
   fsUitls.writeFile(fPath, text)
 }
+
+log('asdad')
 
 module.exports = {log}
