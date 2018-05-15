9e7bd51f3c874f60b15a2b715376527397e50f7c
zenga2 <zenga2@foxmail.com>
2018-05-15 20:11:03 +0800

    测试自动回复

--- src/action/weChat.js
+++ src/action/weChat.js
@@ -1,9 +1,10 @@
 const sha1 = require('sha1')
 const getRawBody = require('raw-body')
+const {parseString} = require('xml2js')
 const config = require('../config')
 const token = config.weixin.token
 
-module.exports = (request, response, ctx) => {
+module.exports = async (request, response, ctx) => {
   const {signature, timestamp, nonce, echostr} = request.query
 
   // 验证微信服务器
@@ -12,17 +13,40 @@ module.exports = (request, response, ctx) => {
     const strAfterSha1 = sha1(str)
     response.body = strAfterSha1 === signature ? echostr : ''
   } else {
-    replyMessage(request, response, ctx)
+    await replyMessage(request, response, ctx)
   }
 }
 
 
-function replyMessage(request, response, ctx) {
-  getRawBody(ctx.req, {
+async function replyMessage(request, response, ctx) {
+  const xmlText = await getRawBody(ctx.req, {
     length: ctx.req.headers['content-length'],
     limit: '1mb',
     encoding: 'utf8'
-  }).then(data => {
-    console.log(data)
   })
+
+  // 该方法是同步的
+  parseString(xmlText, {trim: true}, (err, result) => {
+    if (err) throw err
+
+    const msgData = formateWeChatMsg(result)
+    response.body = `<xml>
+            <ToUserName><![CDATA[${msgData.FromUserName}]]></ToUserName>
+            <FromUserName><![CDATA[${msgData.ToUserName}]]></FromUserName>
+            <CreateTime>${+new Date()}</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType> 
+            <Content><![CDATA[Hello, ${msgData.Content}]]></Content>
+          </xml>`
+  })
+
+}
+
+function formateWeChatMsg(msgData) {
+  msgData = msgData.xml
+
+  return Object.keys(msgData).reduce((result, prop) => {
+    const val = msgData[prop]
+    result[prop] = Array.isArray(val) ? val[0] : val
+    return result
+  }, {})
 }
