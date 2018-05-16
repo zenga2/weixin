ac5c2cadbd69dc4a4431e6d1bf041a96989808cc
zenga2 <zenga2@foxmail.com>
2018-05-16 17:16:08 +0800

    增加获取jsApiTicket的方法

--- src/plugins/localData/index.js
+++ src/plugins/localData/index.js
@@ -4,6 +4,7 @@ const fsUtils = require('../../utils/fsUtils')
 const config = require('../../config')
 
 const dataRootDir = path.join(config.projectRootDir, 'w_data')
+
 // 检测w_data目录是否存在, 不存在就创建一个
 if (!fs.existsSync(dataRootDir)) {
   fs.mkdirSync(dataRootDir)
@@ -16,11 +17,21 @@ async function loadJsonData(filename) {
     : JSON.parse(await fsUtils.readFile(fPath))
 }
 
-function saveJsonDate(data, filename) {
+// 如果先data = await loadJsonData, 然后修改data，同时中间进行了异步操作
+// 最后saveJsonData(data)。这种情况可能会出现原文件已被其他函数修改的问题
+// 原因是中间的异步操作把执行权让给了其他函数
+// 如果其他函数正好修改了原文件，可能会出现非预期的结果
+async function saveJsonData(data, filename) {
   const fPath = path.join(dataRootDir, filename)
-  fsUtils.writeFile(fPath, JSON.stringify(data))
+  await fsUtils.writeFile(fPath, JSON.stringify(data))
+}
+
+// 注意是浅拷贝
+async function updateJsonDate(updateData, filename) {
+  const newData = Object.assign(await loadJsonData(filename), updateData)
+  await saveJsonData(newData, filename)
 }
 
 module.exports = {
-  loadJsonData, saveJsonDate
+  loadJsonData, saveJsonData, updateJsonDate
 }
--- src/plugins/wechat/index.js
+++ src/plugins/wechat/index.js
@@ -1,25 +1,26 @@
 const axios = require('axios')
+const sha1 = require('sha1')
 const config = require('../../config/index')
 const {objToString} = require('../../utils/index')
-const {loadJsonData, saveJsonDate} = require('../localData/index')
+const {loadJsonData, updateJsonDate} = require('../localData/index')
 
 const filename = 'wechat.json'
 
 async function getAccessToken() {
   let data = await loadJsonData(filename)
   const now = +new Date()
+  let accessToken, accessTokenExpires
 
   // 不存在accessToken，或已经失效，就向微信服务请求接口
-  if (!data.accessToken || data.expires < now) {
-    const weChatData = await queryAccessToken()
-    data.accessToken = weChatData.access_token
+  if (!data.accessToken || data.accessTokenExpires < now) {
+    const {access_token, expires_in} = await queryAccessToken()
+    accessToken = access_token
     // 提前五分钟重新请求
-    data.expires = (+new Date()) + (weChatData.expires_in - 300) * 1000
-    // 保存数据
-    saveJsonDate(data, filename)
+    accessTokenExpires = (+new Date()) + (expires_in - 300) * 1000
+    await updateJsonDate({accessToken, accessTokenExpires}, filename)
   }
 
-  return data.accessToken
+  return accessToken
 }
 
 async function queryAccessToken() {
@@ -40,6 +41,78 @@ async function queryAccessToken() {
   return data
 }
 
+async function getJsApiConfigData(url) {
+  const jsapi_ticket = await getJsApiTicket()
+  const noncestr = createNonceStr()
+  const timestamp = createTimestamp()
+  url = url.split('#')[0]
+
+  const checkStr = `jsapi_ticket=${jsapi_ticket}&noncestr=${noncestr}&timestamp=${timestamp}&url=${url}`
+  const signature = sha1(checkStr)
+
+  return {noncestr, timestamp, signature}
+}
+
+async function getJsApiTicket() {
+  let data = await loadJsonData(filename)
+  const now = +new Date()
+  let jsApiTicket, jsApiTicketExpires
+
+  if (!data.jsApiTicket || data.jsApiTicketExpires < now) {
+    const {ticket, expires_in} = await queryJsApiTicket()
+    jsApiTicket = ticket
+    jsApiTicketExpires = (+new Date()) + (expires_in - 300) * 1000
+    await updateJsonDate({jsApiTicket, jsApiTicketExpires}, filename)
+  }
+
+  return data
+}
+
+async function queryJsApiTicket() {
+  const access_token = await getAccessToken()
+  const {data} = await axios.request({
+    url: 'https://api.weixin.qq.com/cgi-bin/ticket/getticket',
+    method: 'get',
+    params: {
+      access_token,
+      type: 'jsapi'
+    }
+  })
+
+  if (!data.ticket) {
+    throw new Error(`从微信获取JsApiTicket不成功: ${objToString(data)}`)
+  }
+
+  return data
+}
+
+// 生成随机字符串
+function createNonceStr(strLen = 20) {
+  const code_0 = 48
+  const code_9 = 57
+  const code_A = 65
+  const code_Z = 90
+  const code_a = 97
+  const code_z = 122
+  const codeArr = []
+
+  while (codeArr.length < strLen) {
+    const code = Math.floor(Math.random() * 123)
+    if (code_0 <= code && code <= code_9
+      || code_A <= code && code <= code_Z
+      || code_a <= code && code <= code_z
+    ) codeArr.push(code)
+  }
+
+  return String.fromCharCode(...codeArr)
+}
+
+// 生成时间戳字符串
+function createTimestamp() {
+  return Math.round((+new Date()) / 1000) + ''
+}
+
 module.exports = {
-  getAccessToken
+  getAccessToken,
+  getJsApiConfigData
 }
