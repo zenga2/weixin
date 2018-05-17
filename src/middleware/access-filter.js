b828ad0c1a4c2f31db76e0d43843df8cafbcbacf
zenga2 <zenga2@foxmail.com>
2018-05-17 16:36:03 +0800

    fix bug

--- src/middleware/access-filter.js
+++ src/middleware/access-filter.js
@@ -4,7 +4,7 @@ const {objToString} = require('../utils/utils')
 module.exports = () => async (ctx, next) => {
   const {path, method, query, href} = ctx.request
   // 打印访问日志
-  log(`path: ${path}; method: ${method}; ${objToString(query)}; href: ${href}; from: ${}`)
+  log(`path: ${path}; method: ${method}; ${objToString(query)}; href: ${href}; from:`)
 
   await next()
 }
