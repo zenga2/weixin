c80b8a14ed9fa1ca640468dc3198ac52595204e4
zenga2 <zenga2@foxmail.com>
2018-05-19 15:06:54 +0800

    打印headers

--- src/middleware/access-filter.js
+++ src/middleware/access-filter.js
@@ -1,10 +1,17 @@
 const {log} = require('../plugins/log')
 const {objToString} = require('../utils/utils')
 
+function headersToString(headers) {
+  return Object.keys(headers).reduce((result, key) => {
+    return result + headers[key] + '\n'
+  }, '')
+}
+
 module.exports = () => async (ctx, next) => {
   const {path, method, query, href, ip} = ctx.request
   // 打印访问日志
-  log(`path: ${path}; method: ${method}; ${objToString(query)}; queryUrl: ${href}; from: ${ip}\n ${ctx.req.headers}`)
+  log(`path: ${path}; method: ${method}; ${objToString(query)}; queryUrl: ${href}; from: ${ip}\n ${headersToString(ctx.req.headers)}`)
+
 
   await next()
 }
