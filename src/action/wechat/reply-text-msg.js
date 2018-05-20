b014dce742d125ee0953d67144d7e203ea2d5b0e
zenga2 <zenga2@foxmail.com>
2018-05-20 12:15:48 +0800

    测试

--- src/action/wechat/reply-text-msg.js
+++ src/action/wechat/reply-text-msg.js
@@ -12,25 +12,27 @@ module.exports = async msgData => {
       break
     // 回复图片消息
     case '2':
-      data = {msgType: 'image', mediaId: await upload('image', 'static/img.jpg')}
+      data = {msgType: 'image', image: [{mediaId: await upload('image', 'static/img.jpg')}]}
       break
     // 回复语音消息
     case '3':
-      data = {msgType: 'voice', mediaId: await upload('voice', 'static/music.mp3')}
+      data = {msgType: 'voice', voice: [{mediaId: await upload('voice', 'static/music.mp3')}]}
       break
     // 回复视频消息
     case '4':
-      data = {msgType: 'video', mediaId: await upload('video', 'static/video.mp4'), title: '精彩的视频', description: '精彩的视频的描述'}
+      data = {msgType: 'video', video: [{mediaId: await upload('video', 'static/video.mp4')}], title: '精彩的视频', description: '精彩的视频的描述'}
       break
     // 回复音乐消息
     case '5':
       data = {
         msgType: 'music',
-        title: '胜似天籁',
-        description: '美妙绝伦的音乐',
-        musicURL: await upload('voice', 'static/music.mp3'),
-        hQMusicUrl: await upload('voice', 'static/music.mp3'),
-        thumbMediaId: await upload('image', 'static/img.jpg')
+        music: [{
+          title: '胜似天籁',
+          description: '美妙绝伦的音乐',
+          musicURL: 'http://47.106.77.181/static/music.mp3',
+          hQMusicUrl: 'http://47.106.77.181/static/music.mp3',
+          thumbMediaId: await upload('image', 'static/img.jpg')
+        }]
       }
       break
     // 回复图文消息
@@ -39,8 +41,8 @@ module.exports = async msgData => {
         msgType: 'news',
         articleCount: 2,
         articles: [
-          {title: '上叠福路口', description: '上叠福路口', picUrl: 'http://47.106.77.181/static/images/img.jpg', url: 'https://www.baidu.com'},
-          {title: '开始的减肥收快递费', description: '萨达', picUrl: 'http://47.106.77.181/static/images/img.jpg', url: 'http://es6.ruanyifeng.com'}
+          {title: '上叠福路口', description: '上叠福路口', picUrl: 'http://47.106.77.181/static/img.jpg', url: 'https://www.baidu.com'},
+          {title: '开始的减肥收快递费', description: '萨达', picUrl: 'http://47.106.77.181/static/img.jpg', url: 'http://es6.ruanyifeng.com'}
         ]
       }
       break
