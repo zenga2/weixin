a7b7b82f421b6a6f2317a3b33d9ca76d5fd886fa
zenga2 <zenga2@foxmail.com>
2018-05-20 11:39:27 +0800

    测试

--- src/action/wechat/reply-text-msg.js
+++ src/action/wechat/reply-text-msg.js
@@ -12,15 +12,15 @@ module.exports = async msgData => {
       break
     // 回复图片消息
     case '2':
-      data = {msgType: 'image', mediaId: await upload('static/img.jpg')}
+      data = {msgType: 'image', mediaId: await upload('image', 'static/img.jpg')}
       break
     // 回复语音消息
     case '3':
-      data = {msgType: 'voice', mediaId: await upload('static/music.mp3')}
+      data = {msgType: 'voice', mediaId: await upload('voice', 'static/music.mp3')}
       break
     // 回复视频消息
     case '4':
-      data = {msgType: 'video', mediaId: await upload('static/video.mp4'), title: '精彩的视频', description: '精彩的视频的描述'}
+      data = {msgType: 'video', mediaId: await upload('video', 'static/video.mp4'), title: '精彩的视频', description: '精彩的视频的描述'}
       break
     // 回复音乐消息
     case '5':
@@ -28,9 +28,9 @@ module.exports = async msgData => {
         msgType: 'music',
         title: '胜似天籁',
         description: '美妙绝伦的音乐',
-        musicURL: await upload('static/music.mp3'),
-        hQMusicUrl: await upload('static/music.mp3'),
-        thumbMediaId: await upload('static/img.jpg')
+        musicURL: await upload('voice', 'static/music.mp3'),
+        hQMusicUrl: await upload('voice', 'static/music.mp3'),
+        thumbMediaId: await upload('image', 'static/img.jpg')
       }
       break
     // 回复图文消息
